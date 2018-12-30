4fb018ee441c99ad426538279270072982574a72
Ryan Hallisey <rhallis1@student.fitchburgstate.edu>
2018-12-30 15:54:51 -0500

    Previous Commit Was Mistake

---
+++ Assets/Scripts/ScaleUp.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleUp : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 5;
+
+    // Use this for initialization
+    void Start () {
+
+        transform.localScale = Vector3.zero;
+        StartCoroutine(ScaleWide());
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+    IEnumerator ScaleWide()
+    {
+        while(transform.localScale.x < .98f)
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, (Vector3.one).Flat(), Time.deltaTime * speed);
+            yield return null;
+        }
+
+        transform.localScale = Vector3.one.Flat();
+        StartCoroutine(ScaleTall());
+      
+    }
+
+    IEnumerator ScaleTall()
+    {
+        
+        while(transform.localScale.y < .98f)
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, Vector3.one, Time.deltaTime * speed);
+            yield return null;
+        }
+
+        transform.localScale = Vector3.one;
+
+
+    }
+
+}
